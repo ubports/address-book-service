0d99b34803f62f95028b1d94dc018ce918aa381a
Matthew Fischer <matthew.fischer@canonical.com>
2013-08-22 14:57:50 -0600

    Removing way outdated dbus schema files since they don't appear to be used. Make contacts.py python3 ready and remove the address fields which will kill the service.

--- examples/contacts.py
+++ examples/contacts.py
@@ -29,9 +29,7 @@ TITLE:Shrimp Man
 PHOTO;VALUE=URL;TYPE=GIF:http://www.example.com/dir_photos/my_photo.gif
 TEL;TYPE=WORK,VOICE:(111) 555-1212
 TEL;TYPE=HOME,VOICE:(404) 555-1212
-ADR;TYPE=WORK:;;100 Waters Edge;Baytown;LA;30314;United States of America
 LABEL;TYPE=WORK:100 Waters Edge\nBaytown, LA 30314\nUnited States of America
-ADR;TYPE=HOME:;;42 Plantation St.;Baytown;LA;30314;United States of America
 LABEL;TYPE=HOME:42 Plantation St.\nBaytown, LA 30314\nUnited States of America
 EMAIL;TYPE=PREF,INTERNET:forrestgump@example.com
 REV:2008-04-24T19:52:43Z
@@ -46,17 +44,17 @@ class Contacts(object):
 
     def connect(self):
         self.bus = dbus.SessionBus()
-        self.addr = self.bus.get_object('com.canonical.galera',
-                                        '/com/canonical/galera/AddressBook')
+        self.addr = self.bus.get_object('com.canonical.pim',
+                                        '/com/canonical/pim/AddressBook')
         self.addr_iface = dbus.Interface(self.addr,
-                                         dbus_interface='com.canonical.galera.AddressBook')
+                                         dbus_interface='com.canonical.pim.AddressBook')
 
     def query(self, fields = '', query = '', sources = []):
         view_path = self.addr_iface.query(fields, query, [])
-        view = self.bus.get_object('com.canonical.galera',
+        view = self.bus.get_object('com.canonical.pim',
                                    view_path)
         view_iface = dbus.Interface(view,
-                                    dbus_interface='com.canonical.galera.View')
+                                    dbus_interface='com.canonical.pim.View')
         contacts = view_iface.contactsDetails([], 0, -1)
         view.close()
         return contacts
@@ -76,7 +74,7 @@ service.connect()
 
 if "query" in sys.argv:
     for contact in service.query():
-        print contact
+        print (contact)
 
 if "update" in sys.argv:
     vcard = VCARD_JOE
@@ -84,12 +82,12 @@ if "update" in sys.argv:
     vcard = vcard.replace("VERSION:3.0", "VERSION:3.0\nUID:%s" % (contactId))
     vcard = vcard.replace("N:Gump;Forrest", "N:Hanks;Tom")
     vcard = vcard.replace("FN:Forrest Gumpt", "N:Tom Hanks")
-    print service.update(vcard)
+    print (service.update(vcard))
 
 if "create" in sys.argv:
-    print "New UID:", service.create(VCARD_JOE)
+    print ("New UID:", service.create(VCARD_JOE))
 
 if "delete" in sys.argv:
     vcard = VCARD_JOE
     contactId = service.create(vcard)
-    print service.delete([contactId])
+    print (service.delete([contactId]))
