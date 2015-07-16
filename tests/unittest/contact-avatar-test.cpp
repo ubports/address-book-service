91462f7dad579100a049c671ee4f23bc16966820
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2015-07-16 18:01:40 -0300

    Created more tests for avatar update use case.

--- tests/unittest/contact-avatar-test.cpp
+++ tests/unittest/contact-avatar-test.cpp
@@ -88,7 +88,7 @@ private Q_SLOTS:
         QVERIFY(avatar.imageUrl().toString().startsWith(QStringLiteral("file:///tmp/contact-avatar-test-")));
     }
 
-    void testCreateModifyAvatarLocally()
+    void testModifyAvatarLocally()
     {
         QContact contact = galera::VCardParser::vcardToContact(QString("BEGIN:VCARD\r\n"
                                                                        "VERSION:3.0\r\n"
@@ -137,6 +137,74 @@ private Q_SLOTS:
         }
         // Avatar revision must be reseted during a loca update
         QVERIFY(avatarRev.data().toString().isEmpty());
+
+        // Avatar should have a new value
+        avatar = newContact.detail<QContactAvatar>();
+        QVERIFY(!avatar.imageUrl().isEmpty());
+        QVERIFY(avatar.imageUrl() != oldAvatar);
+    }
+
+    void testModifyAvatarAndVersion()
+    {
+        QContact contact = galera::VCardParser::vcardToContact(QString("BEGIN:VCARD\r\n"
+                                                                       "VERSION:3.0\r\n"
+                                                                       "X-REMOTE-ID:1dd7d51a1518626a\r\n"
+                                                                       "N:;Fulano;;;\r\n"
+                                                                       "EMAIL:fulano@gmail.com\r\n"
+                                                                       "TEL:123456\r\n"
+                                                                       "X-AVATAR-REV:1234567890\r\n"
+                                                                       "CLIENTPIDMAP:56183a5b-5da7-49fe-8cf6-9bfd3633bf6d\r\n"
+                                                                       "PHOTO;VALUE=URL:file://%1/avatar.svg\r\n"
+                                                                       "END:VCARD\r\n").arg(TEST_DATA_DIR));
+
+        // create a contact
+        QSignalSpy spyContactAdded(m_manager, SIGNAL(contactsAdded(QList<QContactId>)));
+        bool result = m_manager->saveContact(&contact);
+        QCOMPARE(result, true);
+        QTRY_COMPARE(spyContactAdded.count(), 1);
+
+        // qery contacts;
+        QList<QContact> contacts = m_manager->contacts();
+        QCOMPARE(contacts.size(), 1);
+
+        // update a contact
+        QContact newContact(contacts[0]);
+        QContactAvatar avatar = newContact.detail<QContactAvatar>();
+        QUrl oldAvatar = avatar.imageUrl();
+        avatar.setImageUrl(QUrl::fromLocalFile(QString("%1/new_avatar.svg").arg(TEST_DATA_DIR)));
+        newContact.saveDetail(&avatar);
+
+        // udate avatar version
+        QContactExtendedDetail avatarRev;
+        Q_FOREACH(const QContactExtendedDetail &det, newContact.details<QContactExtendedDetail>()) {
+            if (det.name() == "X-AVATAR-REV") {
+                avatarRev = det;
+            }
+        }
+        QString avatarRevValue(QDateTime::currentDateTime().toString(Qt::ISODate));
+        avatarRev.setData(avatarRevValue);
+        newContact.saveDetail(&avatarRev);
+
+        // save contact
+        QSignalSpy spyContactChanged(m_manager, SIGNAL(contactsChanged(QList<QContactId>)));
+        result = m_manager->saveContact(&newContact);
+        QCOMPARE(result, true);
+        QTRY_COMPARE(spyContactChanged.count(), 1);
+
+        // query the updated contact
+        contacts = m_manager->contacts();
+        QCOMPARE(contacts.size(), 1);
+        newContact = contacts[0];
+
+        // check if contact was updated
+        avatarRev = QContactExtendedDetail();
+        Q_FOREACH(const QContactExtendedDetail &det, newContact.details<QContactExtendedDetail>()) {
+            if (det.name() == "X-AVATAR-REV") {
+                avatarRev = det;
+            }
+        }
+        // Avatar revision must be updated to the new value
+        QCOMPARE(avatarRev.data().toString(), avatarRevValue);
 
         // Avatar should have a new value
         avatar = newContact.detail<QContactAvatar>();
